aac37e8e596ac077593ca6c68f0ff7c93b09aaf6
Linh Dan Tran <linhdan23041997@yahoo.com.vn>
2018-03-03 21:11:44 +0700

    Create roy-and-ropes.cpp

---
+++ hackerearth/dynamic-programming/roy-and-ropes.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std; 
+int main(){
+    int t;
+    cin >> t;
+    
+    for (int i= 0; i < t; i++){
+        int n;
+        cin >> n;
+        int max = 0, minute = 1;
+        int ups[n-1];
+        int downs[n-1];
+        for (int j = 0; j < n - 1; j++){
+            cin >> ups[j];
+        }
+        for (int j = 0; j < n - 1; j++){
+            cin >> downs[j];
+        }
+        int plus;
+        for (int j = 0; j < n -1; j++){
+            plus = 0;
+            if (j != n-1){
+                ups[j]--;
+                downs[j]--;
+                plus = ups[j] > downs[j] ? ups[j] : downs[j];
+                if (plus < 0) plus = 0;
+            }
+            minute++;
+            if (minute + plus > max) {
+                max = minute + plus;
+            }
+        }
+        cout << max << endl;
+    }
+    return 0;
+}
